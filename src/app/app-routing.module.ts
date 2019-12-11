e9a1b5116e5ff96adb64432c022041bbd930ed0b
Abid Akram <abid.a@OPN-MC-019.local>
2019-12-11 21:27:16 +0530

    lazy loading

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  { path: 'page1', loadChildren: () => import('./page1/page1.module').then(mod => mod.Page1Module) },
+  { path: 'page2', loadChildren: () => import('./page2/page2.module').then(mod => mod.Page2Module) },
+  { path: 'page3', loadChildren: () => import('./page3/page3.module').then(mod => mod.Page3Module) },
+  { path: 'page4', loadChildren: () => import('./page4/page4.module').then(mod => mod.Page4Module) },
+  { path: 'page5', loadChildren: () => import('./page5/page5.module').then(mod => mod.Page5Module) },
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/page5/page5-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { Page5Component } from './page5.component';
+
+const routes: Routes = [{ path: '', component: Page5Component }];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class Page5RoutingModule { }
